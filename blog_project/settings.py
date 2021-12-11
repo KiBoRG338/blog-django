745501bacbef88db4377315a0c3d77125c1c038e
kiborg338 <koval.viktor338@gmail.com>
2021-12-11 15:43:27 +0200

    added reg and login

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -131,7 +131,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig', 
+    'accounts.apps.AccountsConfig'
 ]
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home' 
